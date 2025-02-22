69e5cb7f29240ffcb7451939d7c658832eb2e313
Zachary Cuddy <zcuddy23@gmail.com>
2025-02-21 22:38:38 -0600

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <main id="app">
+    <nuxt />
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'DefaultLayout',
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <article>
+    <h1>Loftus International</h1>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
