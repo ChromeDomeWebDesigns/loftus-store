3ec247020f3889577fa8d0ccd21b09034d397d79
Zachary Cuddy <info@chromedomewebdesigns.com>
2025-02-28 00:20:21 -0600

    our picks

--- components/landing/OurPicks.vue
+++ components/landing/OurPicks.vue
@@ -1,6 +1,24 @@
 <template>
   <section class="py-12 bg-primary text-white">
     <LoftusSubHeading class="mb-8">Our Picks</LoftusSubHeading>
-    <p class="text-center">TODO: Add products</p>
+    <LoftusShopItemList :items="ourPicks" />
   </section>
 </template>
+
+<script setup>
+  import { computed } from 'vue'
+  import { useCollectionsStore } from '~/store/collections'
+
+  const CollectionsStore = useCollectionsStore()
+  const { collectionId } = defineProps(['collectionId'])
+
+  const ourPicks = computed(() => {
+    const collection = CollectionsStore.getCollectionById(collectionId)
+
+    if (!collection) {
+      return []
+    }
+
+    return [collection.items[7], collection.items[8], collection.items[9], collection.items[10]]
+  })
+</script>
