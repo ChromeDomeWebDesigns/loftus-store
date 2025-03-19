069b5cf90a24f308eb79651cd66c77505fd857e1
Zachary Cuddy <info@chromedomewebdesigns.com>
2025-03-18 23:16:44 -0500

    Inflate cart items

--- store/cart.js
+++ store/cart.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
 import Cookie from 'js-cookie'
 import { CART_COOKIE } from '@/lib/constants'
+import { useItemsStore } from '@/store/items'
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
@@ -9,6 +10,9 @@ export const useCartStore = defineStore('cart', {
   getters: {
     getCart(state) {
       return Object.values(state.cart)
+    },
+    getItemsInCart(state) {
+      return Object.keys(state.cart)
     },
     isCartEmpty(state) {
       return Boolean(Object.values(state.cart).length <= 0)
@@ -44,12 +48,16 @@ export const useCartStore = defineStore('cart', {
     loadCartFromCookie(cart) {
       try {
         this.LOAD_CART_FROM_COOKIE(JSON.parse(cart))
+
+        this.inflateCartItems(Object.keys(this.cart))
       } catch {
         this.LOAD_CART_FROM_COOKIE({})
       }
     },
     updateCart ({ itemId, quantity = 1 } = {}) {
-      this.UPDATE_CART({ itemId, data: { id: itemId, quantity }})
+      const totalQuantity = this.cart[itemId] ? this.cart[itemId].quantity + quantity : quantity
+
+      this.UPDATE_CART({ itemId, data: { id: itemId, quantity: totalQuantity }})
     },
     removeItemFromCart(itemId) {
       this.REMOVE_FROM_CART(itemId)
@@ -57,5 +65,12 @@ export const useCartStore = defineStore('cart', {
     clearCart() {
       this.CLEAR_CART()
     },
+    async inflateCartItems(items) {
+      const ItemsStore = useItemsStore()
+
+      const promises = items.map((itemId => ItemsStore.fetchItemById({ itemId })))
+
+      await Promise.all(promises)
+    }
   },
 })
