969c5ccd9b72e91b4c57e109c9f83ca76c91a8fe
Zachary Cuddy <info@chromedomewebdesigns.com>
2025-04-05 15:34:25 -0500

    Mock form

--- components/products/QuickView.vue
+++ components/products/QuickView.vue
@@ -9,10 +9,10 @@
         <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 md:translate-y-0 md:scale-95" enter-to="opacity-100 translate-y-0 md:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 md:scale-100" leave-to="opacity-0 translate-y-4 md:translate-y-0 md:scale-95">
           <DialogPanel class="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
             <div class="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-              <button type="button" class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8" @click="$emit('close')">
+              <LoftusButton type="button" class="absolute right-4 top-4 text-gray-400 border-0 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8" @click="$emit('close')">
                 <span class="sr-only">Close</span>
                 <XMarkIcon class="size-6" aria-hidden="true" />
-              </button>
+              </LoftusButton>
 
               <ProductsOverview :product="product" />
             </div>
